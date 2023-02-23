79e02b13fac6cfbeaf68a908df44b3072cbd9944
strangecodezZ <strangecodez@protonmail.com>
2023-02-23 05:50:30 -0800

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include <main.h>
-#include <stdio.h>
+
 /**
 * _isupper - checks for uppercase character
 * @c: Variable text
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
