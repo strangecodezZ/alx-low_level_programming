350632d1f41e0376afd3344eec8b69d5253159d5
strangecodezZ <strangecodez@protonmail.com>
2023-02-23 05:56:57 -0800

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,21 @@
-#include <main.h>
+/*
+ * File: 0-isupper.c
+ * 
+ */
+
+#include "main.h"
 
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
-*/
-
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
-	return (1);
-	}
+		return (1);
+
 	else
-	{
-	return (0);
-	}
+		return (0);
 }
